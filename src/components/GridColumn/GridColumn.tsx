7de3e53b2b63dfdd9a78d31625be56af871d0082
gretawix <gretav@wix.com>
2024-09-04 18:23:08 +0300

    refactor

--- src/components/GridColumn/GridColumn.tsx
+++ src/components/GridColumn/GridColumn.tsx
@@ -39,11 +39,4 @@ const GridColumn: React.FC<GridColumnProps> = ({ columnId, isNewEvent }) => {
   );
 };
 
-const areEqual = (prevProps: GridColumnProps, nextProps: GridColumnProps) => {
-  return (
-    prevProps.isNewEvent === nextProps.isNewEvent &&
-    prevProps.columnId === nextProps.columnId
-  );
-};
-
-export default memo(GridColumn, areEqual);
+export default memo(GridColumn);
